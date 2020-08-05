200c12145ffcd17e8a7e47796ea42995612d84fc
marcelogjg <69175942+marcelogjg@users.noreply.github.com>
2020-08-05 13:59:48 -0300

    22
    22

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*11111111111aaaaaaaa111*/
+/*11111111111aaaaaaaaa111*/
 
 Route::get('/', function () {
     return view('welcome');
